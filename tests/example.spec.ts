362b547115eb24ef9db3d8f5c2f0f69efb7c206b
Tetiana778 <58707437+Tetiana778@users.noreply.github.com>
2025-08-15 13:33:48 +0300

    Negative test was added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,9 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test('Login Positive', async ({ page }) => {
   await page.goto('http://localhost:5000/');
-await page.locator('input#login').fill('test');
-await page.locator('input#password').fill('newyork1');
-await page.locator('button#login').click();
+  await page.locator('input#login').fill('test');
+  await page.locator('input#password').fill('newyork1');
+  await page.locator('button#login').click();
 
-await expect( page.getByText('calculate')).toBeVisible();
+  await expect( page.getByText('calculate')).toBeVisible();
+});
+
+
+test('Login with Empty Fields', async ({ page }) => {
+  await page.goto('http://localhost:5000/');
+  await page.locator('button#login').click();
+
+  await expect( page.locator('#errorMessage')).toHaveText('User not found!');
 });
